f393d9cf57ea1c2e96b745596dff94a4d180cc5f
Jonathan Dunlap <jdunlap@outlook.com>
2016-02-14 21:15:45 -0800

    wip mocha - blocked - mocha forks

--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
+'use strict'
 var Rx = require('rx');
 var rc = require('../dist/rxjs-cluster.js');
+var assert = require('assert');
 
 var Observable = Rx.Observable;
 
@@ -10,32 +12,52 @@ function childTest(x) {
 
 // Child function that returns an Observable
 function childTest$(x) {
-	return Rx.Observable.range(0,3).map("hello " + x + " from " + process.pid).toArray();
+	return Observable.range(0,3).map("hello " + x + " from " + process.pid).toArray();
 }
 
 function master() {
-	Observable.from(['Jonathan', 'James', 'Edwin'])
-		.clusterMap('childTest')
-		.subscribe( 
-			function(x) { console.log(x); },
-			function(x) { console.log('Err ' + x); },
-			function() { console.log('Completed'); }
-		);
-
-	Observable.from(['Jonathan', 'James', 'Edwin'])
-		.clusterMap('childTest$')
-		.subscribe( 
-			function(x) { console.log(x); },
-			function(x) { console.log('Err ' + x); },
-			function() { 
-				console.log('Completed'); 
-				rc.killall(); // kill all workers, clusterMap will no longer work
-			}
-		);
+	it('workers returns primitive value', done => {
+		Observable.from(['Jonathan', 'James', 'Edwin'])
+			.clusterMap('childTest')
+			.toArray()
+			.subscribe( 
+				function(x) { 
+					assert.equal(x.length, 3);
+					console.log(x); 
+				},
+				function(x) { console.log('Err ' + x); },
+				function() { console.log('Completed'); done(); }
+			);
+  	})
+	
+	it('set entry', done => {
+		Observable.from(['Jonathan', 'James', 'Edwin'])
+			.clusterMap('childTest$')
+			.toArray()
+			.subscribe( 
+				function(x) { console.log(x); },
+				function(x) { console.log('Err ' + x); },
+				function() { 
+					console.log('Completed'); 
+					done();
+					rc.killall(); // kill all workers, clusterMap will no longer work
+				}
+			);
+		});
 }
 
-// Define number of workers, master entry point, worker functions
-rc.entry(3, master, { childTest: childTest, childTest$: childTest$ });
+function tests() {
+	console.log('tests started')
+	describe('# local Cluster work', () => {
+		master();
+	});
+}
+//console.log('-', module.parent.id);
+describe('## rxjs-cluster', () => {
+	rc.entry(3, tests, { childTest: childTest, childTest$: childTest$ })
+})
+
+// Define number of workers, master entry point, worker functions$ });
 
 // Or define leave the default number of workers to # of cpu cores
 // rc.entry(master, { childTest: childTest, childTest$: childTest$ });
